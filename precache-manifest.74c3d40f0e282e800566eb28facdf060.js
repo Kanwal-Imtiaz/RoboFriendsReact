255b496f9f3a162dea4d4df63d40ca8da3ce69cd
Kanwal-Itmiaz <kanwalimtiaz94@gmail.com>
2019-10-08 19:01:11 +1100

    Updates

---
+++ precache-manifest.74c3d40f0e282e800566eb28facdf060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2015f1ad0e72978e3d62517b7f7ccc8",
+    "url": "/RoboFriendsReact/index.html"
+  },
+  {
+    "revision": "111beafd40802a036d79",
+    "url": "/RoboFriendsReact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "17c4c418944185fa5fa5",
+    "url": "/RoboFriendsReact/static/css/main.620cd86d.chunk.css"
+  },
+  {
+    "revision": "111beafd40802a036d79",
+    "url": "/RoboFriendsReact/static/js/2.eca0a466.chunk.js"
+  },
+  {
+    "revision": "17c4c418944185fa5fa5",
+    "url": "/RoboFriendsReact/static/js/main.87e56550.chunk.js"
+  },
+  {
+    "revision": "f5b5e7ed8f92a6769a7c",
+    "url": "/RoboFriendsReact/static/js/runtime-main.10eb449b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsReact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
